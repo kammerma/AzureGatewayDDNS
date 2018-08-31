f424f1bee09ffbf715ff79643e73a85fb96c8465
kammerma <42848231+kammerma@users.noreply.github.com>
2018-08-31 20:14:55 +0200

    Updated AzureGatewayDDNS.ps1 in Azure Automation.

--- Azure Automation/AzureGatewayDDNS.ps1
+++ Azure Automation/AzureGatewayDDNS.ps1
@@ -33,9 +33,9 @@ catch {
 
 #Get the current IPs (local dynamic & gateway)
 [string]$dynamicIP = ([System.Net.DNS]::GetHostAddresses($dynamicFQDN)).IPAddressToString
-Write-Output "Current Dynamic IP: " + $dynamicIP
+Write-Output "Current Dynamic IP: $dynamicIP"
 $localGateway = Get-AzureRmLocalNetworkGateway -ResourceName $gatewayName -ResourceGroupName $resourceGroup
-Write-Output "Current Local Network Gateway IP: " + $localGateway.GatewayIPAddress
+Write-Output "Current Local Network Gateway IP: $($localGateway.GatewayIPAddress)"
 
 #Determine if gateway IP needs update
 If ($dynamicIP -ne $localGateway.GatewayIpAddress) {
